758a38b66de11fe8472cb86ba8925a93c0c81114
Johandry loyo <johanloyo6@gmail.com>
2019-05-19 17:45:32 -0400

    Raiz-simple-webpack

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="app">
+		<titulo></titulo>
+	</div>
+
+
+
+
+
+</template>
+
+<script>
+import titulo from './title'
+export default {
+	
+	components:{
+		titulo
+	},
+
+	data() {
+		return {
+			
+		}
+	},
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/title.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>{{titulo}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            titulo: 'Este es un titulo importado'
+        }
+    },
+}
+</script>
