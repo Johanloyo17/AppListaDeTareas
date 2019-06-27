b468367ac7f9d8bdb3d013b57a85da16d2444e32
Johandry loyo <johanloyo6@gmail.com>
2019-06-27 02:30:52 -0400

    Controlando Sing in y Sing up con Slot y Transition

---
+++ src/navegation/dataForm/form-div.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div>
+            <slot name="titulo"></slot>
+            
+                    <slot name="password" ></slot>
+                    <slot name="email" ></slot>
+                    <slot></slot>
+                    <slot></slot>
+                    <slot></slot>
+                    <slot name="forgot-text" ></slot>
+                
+                <slot name="boton"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+    data() {
+        return {
+            showAuth: true
+        }
+    },
+    methods:{
+    
+    },
+    destroyed() {
+        console.log("se destuyo todo, todo, todillo")
+    },
+}
+</script>
+
+<style>
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-down-enter-active {
+    transition: all .3s ease;
+}
+.slide-down-leave-active {
+    transition: all .4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-down-enter, .slide-down-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+    transform: translateY(-20px);
+    opacity: 0;
+}
+</style>
--- src/navegation/nav-bar.vue
+++ src/navegation/nav-bar.vue
@@ -1,46 +1,21 @@
 <template>
     <div>
-        <nav>
-			<div class="nav-wrapper">
-			<a href="#" class="brand-logo">
-                <!-- <img 
-                src="../asets/saveNotes-logo-01" alt=""> -->
-            </a>
-			<ul id="nav-mobile" class="right hide-on-med-and-down">
-				<li  @click="showRegister = !showRegister"><a >Sing up</a></li>
-				<li @click="showAuth = !showAuth"><a>Sing in</a></li>
-                <!-- esta etiqueta evita que se destruya el componente cuando se cierre -->
-                <keep-alive>
-                    <ingresa :showAuth="showAuth"></ingresa>
-                </keep-alive>
-                
-                
-                
-                <!-- <transition name="slide-down">
-                    <div 
-                        class="singIn-cont" 
-                        v-if="showRegister"
-                        >
-                        <div class="auth-title">
-                            <span>Ingresa</span>
-                        </div>
-                        <form >
-                            <div class="inputs-container">
-                                <div class="input-field">
-                                    <input id="email" type="email" class="validate appInput">
-                                    <label for="email">Email</label>
-                                    <span class="helper-text" data-error="Erorr" data-success="Ok">introduce tu correo electronico</span>
-                                </div>
-                                <div class="input-field">
-                                    <input id="password" type="password" class="validate appInput">
-                                    <label for="password">Contraseña</label>
-                                </div>
-                            </div>
-                            
-                        </form>
-                    </div>
-                </transition> -->
-			</ul>
+        <nav class="nav-cont">
+			<div class="nave nav-wrapper">
+                <a href="#" class="brand-logo">
+                </a>
+                <div class="nave-logo">
+                    <img src="../assets/saveNotes-logo.svg" alt="">
+                </div>
+                <ul id="nav-mobile" class="nave-menu right hide-on-med-and-down">
+                    <li  @click="showAuth = !showAuth , componentId = 'signUp' " ><a >Sing up</a></li>
+                    <li @click="showAuth = !showAuth , componentId = 'signIn' "><a>Sing in</a></li>
+                    <!-- esta etiqueta evita que se destruya el componente cuando se cierre -->
+                <transition name="slide-down">
+                    <component class="singIn-cont" v-if="showAuth" :is="componentId"></component>
+                </transition>
+                    <!-- <sign-in></sign-in> -->
+                </ul>
 			</div>
 		</nav>
     </div>
@@ -48,15 +23,20 @@
 
 <script>
 
-import ingresa from './ingresa'
+import formDiv from './dataForm/form-div'
+import signIn from './dataForm/sign-in'
+import signUp from './dataForm/sign-up'
+
 export default {
     components:{
-        ingresa,
+        formDiv: formDiv,
+        signUp: signUp,
+        signIn: signIn,
     },
     data() {
         return {
-            showAuth : true,
-            showRegister : true,
+            showAuth : false,
+            componentId: ""
         }
     },
     
